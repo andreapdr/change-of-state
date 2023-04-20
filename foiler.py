99be034fdc46a3385d0b1153644695c68a4bbaac
andreapdr <andrea.pedrotti@isti.cnr.it>
2023-04-20 16:56:02 +0200

    update foiler
    option to select extended/reduced version + option to select wether to use hypernyms or hyponyms as verbs in the construced foils

--- foiler.py
+++ foiler.py
@@ -14,6 +14,8 @@ PTB_TAG = {
     "VBZ": "3rd person singular present",
 }
 
+REDUCED = False
+
 
 def inflect(verb, tag):
     assert tag in PTB_TAG, f"pos '{tag}' not in {list(tag.keys())}"
@@ -64,63 +66,102 @@ def create_foils(sentence, foil_types=["action"]):
         "inverse": get_inverse_capt_and_foil,
     }
     for ft in foil_types:
-        foils[ft] = switcher[ft](sentence)
+        foils[ft] = switcher[ft](sentence, target="verb-hypernym")
 
     return {"foils": foils}
 
 
-def get_action_capt_and_foil(sentence):
+def get_action_capt_and_foil(sentence, target):
     _object = sentence["object"] if sentence["object"] else "something"
     tag = "VBZ" if not is_plural(_object) else "VBP"
-    _verb, _particle = inflect(sentence["verb"], tag=tag)
-    _verb_passive, _particle_passive = inflect(sentence["verb"], tag="VBZ")
+    _verb, _particle = inflect(sentence[target], tag=tag)
+    _verb_passive, _particle_passive = inflect(sentence[target], tag="VBZ")
     _inverse, _particle_inv = inflect(sentence["state-inverse"], tag=tag)
     _inverse_passive, _particle_inv_passive = inflect(
         sentence["state-inverse"], tag="VBZ"
     )
-
-    if not_transitive(sentence["verb"]):
-        capt = f"{_object.capitalize()} {_verb}{' ' + _particle if _particle else ''}."
-        foil = f"{_object.capitalize()} {_inverse}{' ' + _particle_inv if _particle_inv else ''}."
-    else:
-        capt = f"Someone {_verb_passive} {_object}{' ' + _particle_passive if _particle_passive else ''}."
-        foil = f"Someone {_inverse_passive} {_object}{' ' +_particle_inv_passive if _particle_inv_passive else ''}."
-    return capt, foil
-
-
-def get_prestate_capt_and_foil(sentence):
-    _object = sentence["object"] if sentence["object"] else "Someone"
     aux = manage_aux(_object)
 
-    if not_transitive(sentence["verb"]):
-        capt = f"Initially, {_object} {aux} {sentence['pre-state']}."
-        foil = f"Initially, {_object} {aux} {sentence['post-state']}."
+    if not_transitive(sentence[target]):
+        if REDUCED:
+            capt = (
+                f"{_object.capitalize()} {_verb}{' ' + _particle if _particle else ''}."
+            )
+            foil = f"{_object.capitalize()} {_inverse}{' ' + _particle_inv if _particle_inv else ''}."
+        else:
+            # EXTENDED VERSION
+            capt = f"Initially, {_object} {aux} {sentence['pre-state']}. Then, {_object} {_verb}{' ' + _particle if _particle else ''}. At the end, {_object} {aux} {sentence['post-state']}."
+            foil = f"Initially, {_object} {aux} {sentence['pre-state']}. Then, {_object} {_inverse}{' ' + _particle_inv if _particle_inv else ''}. At the end, {_object} {aux} {sentence['post-state']}."
     else:
-        capt = f"Initially, {_object} {aux} {sentence['pre-state']}."
-        foil = f"Initially, {_object} {aux} {sentence['post-state']}."
+        if REDUCED:
+            capt = f"Someone {_verb_passive} {_object}{' ' + _particle_passive if _particle_passive else ''}."
+            foil = f"Someone {_inverse_passive} {_object}{' ' +_particle_inv_passive if _particle_inv_passive else ''}."
+        else:
+            capt = f"Initially, {_object} {aux} {sentence['pre-state']}. Then, someone {_verb_passive} {_object}{' ' + _particle_passive if _particle_passive else ''}. At the end, {_object} {aux} {sentence['post-state']}."
+            foil = f"Initially, {_object} {aux} {sentence['pre-state']} Then, someone {_inverse_passive} {_object}{' ' + _particle_inv_passive if _particle_inv_passive else ''}. At the end, {_object} {aux} {sentence['post-state']}."
     return capt, foil
 
 
-def get_poststate_capt_and_foil(sentence):
+def get_prestate_capt_and_foil(sentence, target):
     _object = sentence["object"] if sentence["object"] else "Someone"
     aux = manage_aux(_object)
+    _verb, _particle = inflect(sentence[target], tag="VBZ")
+    aux = manage_aux(_object)
 
-    if not_transitive(sentence["verb"]):
-        capt = f"At the end, {_object} {aux} {sentence['post-state']}."
-        foil = f"At the end, {_object} {aux} {sentence['pre-state']}."
+    if not_transitive(sentence[target]):
+        if REDUCED:
+            # REDUCED VERISON
+            capt = f"Initially, {_object} {aux} {sentence['pre-state']}."
+            foil = f"Initially, {_object} {aux} {sentence['post-state']}."
+
+        else:
+            # EXTENDED VERSION
+            capt = f"Initially, {_object} {aux} {sentence['pre-state']}. Then, {_object} {_verb}{' ' + _particle if _particle else ''}. At the end, {_object} {aux} {sentence['post-state']}."
+            foil = f"Initially, {_object} {aux} {sentence['post-state']}. Then, {_object} {_verb}{' ' + _particle if _particle else ''}. At the end, {_object} {aux} {sentence['post-state']}."
     else:
-        capt = f"At the end, {_object} {aux} {sentence['post-state']}."
-        foil = f"At the end, {_object} {aux} {sentence['pre-state']}."
+        if REDUCED:
+            # REDUCED VERSION
+            capt = f"Initially, {_object} {aux} {sentence['pre-state']}."
+            foil = f"Initially, {_object} {aux} {sentence['post-state']}."
+        else:
+            # EXTENDED VERSION
+            capt = f"Initially, {_object} {aux} {sentence['pre-state']}. Then, someone {_verb} {_object}{' ' + _particle if _particle else ''}. At the end, {_object} {aux} {sentence['post-state']}."
+            foil = f"Initially, {_object} {aux} {sentence['post-state']} Then, someone {_verb} {_object}{' ' + _particle if _particle else ''}. At the end, {_object} {aux} {sentence['post-state']}."
     return capt, foil
 
 
-def get_inverse_capt_and_foil(sentence):
-    _verb, _particle = inflect(sentence["verb"], tag="VBZ")
+def get_poststate_capt_and_foil(sentence, target):
+    _object = sentence["object"] if sentence["object"] else "Someone"
+    _verb, _particle = inflect(sentence[target], tag="VBZ")
+    aux = manage_aux(_object)
+
+    if not_transitive(sentence[target]):
+        if REDUCED:
+            capt = f"At the end, {_object} {aux} {sentence['post-state']}."
+            foil = f"At the end, {_object} {aux} {sentence['pre-state']}."
+        else:
+            # EXTENDED VERSION
+            capt = f"Initially, {_object} {aux} {sentence['pre-state']}. Then, {_object} {_verb}{' ' + _particle if _particle else ''}. At the end, {_object} {aux} {sentence['post-state']}."
+            foil = f"Initially, {_object} {aux} {sentence['pre-state']}. Then, {_object} {_verb}{' ' + _particle if _particle else ''}. At the end, {_object} {aux} {sentence['pre-state']}."
+    else:
+        if REDUCED:
+            capt = f"At the end, {_object} {aux} {sentence['post-state']}."
+            foil = f"At the end, {_object} {aux} {sentence['pre-state']}."
+
+        # EXTENDED VERSION
+        else:
+            capt = f"Initially, {_object} {aux} {sentence['pre-state']}. Then, someone {_verb} {_object}{' ' + _particle if _particle else ''}. At the end, {_object} {aux} {sentence['post-state']}."
+            foil = f"Initially, {_object} {aux} {sentence['pre-state']} Then, someone {_verb} {_object}{' ' + _particle if _particle else ''}. At the end, {_object} {aux} {sentence['pre-state']}."
+    return capt, foil
+
+
+def get_inverse_capt_and_foil(sentence, target):
+    _verb, _particle = inflect(sentence[target], tag="VBZ")
     _inverse, _particle_inv = inflect(sentence["state-inverse"], tag="VBZ")
     _object = sentence["object"] if sentence["object"] else "something"
     aux = manage_aux(_object)
 
-    if not_transitive(sentence["verb"]):
+    if not_transitive(sentence[target]):
         capt = f"Initially, {_object} {aux} {sentence['pre-state']}. Then, {_object} {_verb}{' ' + _particle if _particle else ''}. At the end, {_object} {aux} {sentence['post-state']}."
         foil = f"Initially, {_object} {aux} {sentence['post-state']}. Then, {_object} {_inverse}{' ' + _particle_inv if _particle_inv else ''}. At the end, {_object} {aux} {sentence['pre-state']}."
     else:
@@ -134,9 +175,12 @@ if __name__ == "__main__":
     from os.path import expanduser
 
     foil_types = ["action", "pre-state", "post-state", "inverse"]
+    # foil_types = ["pre-state"]
 
     datapath = expanduser("~/datasets/vl-bench/change-state.json")
-    outpath = expanduser("~/datasets/vl-bench/newfoil.json")
+    outpath = expanduser(
+        "~/datasets/vl-bench/change-state-extended-hypernyms-only.json"
+    )
 
     data = json.load(open(datapath))
     print(f"- foiling data at {datapath} - {len(data)} examples")
